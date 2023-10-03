8613e20947c9a288dc3e5c104e2ae18db6f32fb5
Vanessa Mendoza <v.mendoza1@gmail.com>
2023-10-03 13:06:10 -0400

    Added logout functionality to DemoSecurtyConfig and home.html

--- demosecurity/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ demosecurity/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -49,6 +49,8 @@ public class DemoSecurityConfig {
                                 .loginPage("/showMyLoginPage")
                                 .loginProcessingUrl("/authenticateTheUser")
                                 .permitAll()
+                )
+                .logout(logout -> logout.permitAll()
                 );
 
         return http.build();
